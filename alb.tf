3411c71fa941bd0628eb6753b738ded5f08ba9c3
cgpeanut <robertoruizroxas@gmail.com>
2020-10-06 17:59:19 +0000

    changed cgpeanut to smartPension

--- alb.tf
+++ alb.tf
@@ -1,6 +1,6 @@
 resource "aws_lb" "application-lb" {
   provider           = aws.region-master
-  name               = "cgpeanut-jenkins-lb"
+  name               = "smartPension-jenkins-lb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.lb-sg.id]
@@ -10,9 +10,9 @@ resource "aws_lb" "application-lb" {
   }
 }
 
-resource "aws_lb_target_group" "cgpeanut-lb-tg" {
+resource "aws_lb_target_group" "smartPension-lb-tg" {
   provider    = aws.region-master
-  name        = "cgpeanut-lb-tg"
+  name        = "smartPension-lb-tg"
   port        = "80"
   target_type = "instance"
   vpc_id      = aws_vpc.vpc_useast.id
@@ -37,13 +37,13 @@ resource "aws_lb_listener" "jenkins-listener-http" {
   protocol          = "HTTP"
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.cgpeanut-lb-tg.arn
+    target_group_arn = aws_lb_target_group.smartPension-lb-tg.arn
   }
 }
 
 resource "aws_lb_target_group_attachment" "jenkins-master-attach" {
   provider         = aws.region-master
-  target_group_arn = aws_lb_target_group.cgpeanut-lb-tg.arn
+  target_group_arn = aws_lb_target_group.smartPension-lb-tg.arn
   target_id        = aws_instance.jenkins-master.id
   port             = 80
 }
